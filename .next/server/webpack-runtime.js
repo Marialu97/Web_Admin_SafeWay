85fff0710f97e50ba7e7455cfe0f8104e035e6b2
marialu <marialuizaelias04@gmail.com>
2025-11-13 11:02:28 -0300

    texte 2

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("4b28024d5afce34d")
+/******/ 		__webpack_require__.h = () => ("cb430a611db0aa43")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
